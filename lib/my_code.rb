a4211b7affd98d2b75d4f0090cceaaf8f3e1ce73
Muhan Lee <davidmlee4@gmail.com>
2020-01-26 23:13:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map_to_negativize(source_array)
   result = []
   i = 0
   while i < source_array.length do
-    source_array[i] * -1
+    result << source_array[i] * -1
     i += 1
   end
   result
